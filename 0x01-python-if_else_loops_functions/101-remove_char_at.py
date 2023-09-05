82fc22cd42a6cada6dfd4e36bc05ce1357150e6e
Anasoud1 <anasoudsassi@gmail.com>
2023-09-05 11:43:04 -0700

    adding 101-remove_char_at.py file

---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def remove_char_at(str, n):
+    i = 0
+    new_str = ""
+    while i < len(s):
+        if i != n:
+            new_str = new_str + s[i]
+        i += 1
+    return new_str
