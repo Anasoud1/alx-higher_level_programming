13a9dc6964cadc4947b1a3d8bdd5fc60fc89f804
Anasoud1 <anasoudsassi@gmail.com>
2023-09-07 06:40:05 -0700

    adding 1-calculation.py file

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -3,7 +3,11 @@ if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
     a = 10
     b = 5
-    print("10 + 5 = {}".format(add(a, b)))
-    print("10 + 5 = {}".format(sub(a, b)))
-    print("10 + 5 = {}".format(mul(a, b)))
-    print("10 + 5 = {}".format(div(a, b)))
+    add = add(a, b)
+    sub = sub(a, b)
+    mul = mul(a, b)
+    div = div(a, b)
+    print("10 + 5 = {}".format(add))
+    print("10 + 5 = {}".format(sub))
+    print("10 + 5 = {}".format(mul))
+    print("10 + 5 = {}".format(div))
