03d7e240333c4f7032f9b3d1d4054d636004aab4
Anasoud1 <anasoudsassi@gmail.com>
2023-09-07 07:28:39 -0700

    adding 2-args.py file

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,9 +2,12 @@
 import sys
 if len(sys.argv) == 1:
     print("0 arguments.")
+elif len(sys.argv) == 2:
+    print("1 argument:")
+    print("1: {}".format(sys.argv[1]))
 else:
     i = 1
-    print("{} argument:".format(len(sys.argv) - 1))
+    print("{} arguments:".format(len(sys.argv) - 1))
     for av in sys.argv[1:]:
         print("{}: {}".format(i, sys.argv[i]))
         i += 1
