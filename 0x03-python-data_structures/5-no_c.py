c1304f0d62823f679d07399eefae6ddafd489381
Anasoud1 <anasoudsassi@gmail.com>
2023-09-10 06:54:08 -0700

    updating 5-no_c.py file

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,7 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 def no_c(my_string):
     new_string = ""
-    for i in range(len(my_string)):
-        if (my_string[i] != 'c' and my_string[i] != 'C'):
-            new_string += my_string[i]
+    for c in my_string:
+        if c not in ['c', 'C']:
+            new_string += c
     return new_string
