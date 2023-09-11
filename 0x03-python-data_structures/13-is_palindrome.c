20343aa2e8a23550e6814cb29a7569ad8a040e10
Anasoud1 <anasoudsassi@gmail.com>
2023-09-11 09:12:00 -0700

    adding 13-is_palindrome.c, lists.h files

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -6,43 +6,35 @@
  * @head: head of the list
  * Return: 0 if it is not a palindrome, 1 if it is a palindrome
  */
-
 int is_palindrome(listint_t **head)
 {
-	listint_t *start, *curr = *head;
-	int i, l, flag = 0, len = 0;
-	int *tmp;
+        listint_t *start, *curr = *head;
+        int i, j, len = 0;
+        int *tmp;
 
-	while (curr)
-	{
-		curr = curr->next;
-		len++;
-	}
-	if (len % 2 == 0)
-		l = len / 2;
-	else
-		l = len / 2, flag = 1;
-	tmp = malloc(sizeof(int) * (l + 1));
-	if (!tmp)
-		return (-1);
-	start = *head;
-	for (i = 0; i < l; i++)
-	{
-		tmp[i] = start->n;
-		start = start->next;
-	}
-	tmp[i] = '\0';
-	if (flag == 1)
-		start = start->next;
-	for (i = l - 1; i >= 0; i--)
-	{
-		if (tmp[i] != start->n)
-		{
-			free(tmp);
-			return (0);
-		}
-		start = start->next;
-	}
-	free(tmp);
-	return (1);
+        while (curr)
+        {
+                curr = curr->next;
+                len++;
+        }
+        tmp = malloc(sizeof(int) * (len + 1));
+        if (!tmp)
+                return (-1);
+        start = *head;
+        for (i = 0; i < len; i++)
+        {
+                tmp[i] = start->n;
+                start = start->next;
+        }
+        tmp[i] = '\0';
+        for (i = 0, j = len - 1; i < len / 2; i++, j--)
+        {
+                if (tmp[i] != tmp[j])
+                {
+                        free(tmp);
+                        return (0);
+                }
+        }
+        free(tmp);
+        return (1);
 }
