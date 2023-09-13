a4faa12ea927d908e9d9505e4d71d991ee380473
Anasoud1 <anasoudsassi@gmail.com>
2023-09-13 13:28:32 -0700

    adding 13-is_palindrome.c, lists.h files

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,8 +1,32 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    rom = {"I": 1, "II": 2, "III": 3, "IV": 4, "V": 5, "VI": 6, "VII": 7,
-           "VIII": 8, "XI": 9, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+    rom = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
     s = 0
-    for i in roman_string:
-        s += rom[i]
+    i = 0
+    while i < len(roman_string):
+        if (i + 1) < len(roman_string):
+            if roman_string[i] == "C" and roman_string[i + 1] == "D":
+                s += 400
+                i += 2
+            elif roman_string[i] == "C" and roman_string[i + 1] == "M":
+                s += 900
+                i += 2
+            elif roman_string[i] == "X" and roman_string[i + 1] == "L":
+                s += 40
+                i += 2
+            elif roman_string[i] == "X" and roman_string[i + 1] == "C":
+                s += 90
+                i += 2
+            elif roman_string[i] == "I" and roman_string[i + 1] == "V":
+                s += 4
+                i += 2
+            elif roman_string[i] == "I" and roman_string[i + 1] == "X":
+                s += 9
+                i += 2
+            else:
+                s += rom[roman_string[i]]
+                i += 1
+        else:
+            s += rom[roman_string[i]]
+            i += 1
     return s
