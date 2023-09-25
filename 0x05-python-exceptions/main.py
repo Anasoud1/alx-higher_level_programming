1e04008ddb6ea40a136db9a529674e9a913f3769
Anasoud1 <anasoudsassi@gmail.com>
2023-09-25 06:08:46 -0700

    adding 1-safe_print_integer.py file

--- 0x05-python-exceptions/main.py
+++ 0x05-python-exceptions/main.py
@@ -11,7 +11,8 @@ has_been_print = safe_print_integer(value)
 if not has_been_print:
     print("{} is not an integer".format(value))
 
-value = "School"
+value = "m" + 1
+
 has_been_print = safe_print_integer(value)
 if not has_been_print:
     print("{} is not an integer".format(value))
