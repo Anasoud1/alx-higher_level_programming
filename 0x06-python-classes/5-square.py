dfa474d75afd4581028ead35a63bfcd23ce3f870
Anasoud1 <anasoudsassi@gmail.com>
2023-09-26 08:48:16 -0700

    adding 5-square.py file

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+'''Define a class Square'''
+
+
+class Square:
+    '''Define Square'''
+
+    def __init__(self, size=0):
+        '''constructor
+
+        Args:
+           size (int): new size
+        '''
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        '''Return area of square'''
+        return self.__size ** 2
+
+    def my_print(self):
+        '''print # charactere'''
+        if self.__size == 0:
+            print()
+        else:
+            for i in range(self.__size):
+                for j in range(self.__size):
+                    print("#", end="")
+                print()
