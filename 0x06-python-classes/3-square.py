bdf876b13b9460a24e2c8435d69cb0ae81c2b4b4
Anasoud1 <anasoudsassi@gmail.com>
2023-09-26 08:25:39 -0700

    adding 3-square.py file

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+'''Define a class Square'''
+
+
+class Square:
+    '''Define Square'''
+
+    def __init__(self, size):
+        '''Constuctor
+        Args:
+            size (int): new size
+        '''
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        '''Return area of square'''
+        return self.__size * self.__size
