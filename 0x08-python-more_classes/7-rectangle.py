5bfe93beb86cc3fb73807e26371f458a1b9eb8d1
Anasoud1 <anasoudsassi@gmail.com>
2023-10-02 18:15:56 +0100

    adding 7-rectangle.py file

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -60,8 +60,8 @@ class Rectangle:
         '''function print rectangle'''
         if self.width == 0 or self.height == 0:
             return ""
-        return "\n".join(str(self.print_symbol) * self.width\
-                for h in range(self.height))
+        return "\n".join(str(self.print_symbol) * self.width \
+                        for h in range(self.height))
 
     def __repr__(self):
         '''function represente rectangle'''
