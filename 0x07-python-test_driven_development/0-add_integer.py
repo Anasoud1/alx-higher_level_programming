89dc0cd6a738c37f4f1e976eaf45e0df532eb002
Anasoud1 <anasoudsassi@gmail.com>
2023-10-04 12:13:45 +0100

    updating 0-add_integer.py file

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,13 +1,9 @@
 #!/usr/bin/python3
-'''
-Module for add_integer method
-'''
+'''Module for add_integer method'''
 
 
 def add_integer(a, b=98):
-    '''
-    Return addition of 2 number
-    '''
+    '''Return addition of 2 number'''
     if not (isinstance(a, int) or isinstance(a, float)):
         raise TypeError("a must be an integer")
     if not (isinstance(b, int) or isinstance(b, float)):
