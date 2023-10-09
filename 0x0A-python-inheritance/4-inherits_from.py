9f37317bbab19574197383f97167c6834578b367
Anasoud1 <anasoudsassi@gmail.com>
2023-10-09 18:01:34 +0100

    adding 4-inherits_from.py file

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+'''Define a inherits_from methid'''
+
+
+def inherits_from(obj, a_class):
+    '''returns True if the object is an instance of a class that
+    inherited (directly or indirectly) from the specified class;
+    otherwise False.'''
+
+    return not type(obj) == a_class
