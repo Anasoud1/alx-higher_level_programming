d378fe6cff2a90506384596637a7893dd65c53de
Anasoud1 <anasoudsassi@gmail.com>
2023-10-10 13:52:27 +0100

    adding readme and 0-read_file.py files

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+'''Define read_file function'''
+
+
+def read_file(filename=""):
+    '''Reads a text file (UTF8) and prints it to stdout
+    Args:
+        filename: file name
+    '''
+    with open(filename, encoding="utf-8") as f:
+        print(f.read())
