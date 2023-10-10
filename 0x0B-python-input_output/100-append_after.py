e88a6f348cf2869f86eda9f48dcd970f9cbb708c
Anasoud1 <anasoudsassi@gmail.com>
2023-10-10 21:39:54 +0100

    adding 100-append_after.py file

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -12,5 +12,4 @@ def append_after(filename="", search_string="", new_string=""):
             if search_string in line:
                 text += new_string
     with open(filename, "w", encoding="utf-8") as f:
-            f.write(text)
-
+        f.write(text)
