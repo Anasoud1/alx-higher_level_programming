a7473e090c750da7fb868b25bcb724211e6babc4
Anasoud1 <anasoudsassi@gmail.com>
2023-10-10 16:41:20 +0100

    adding 7-add_item.py file

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import sys
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+
+if len(sys.argv) == 1:
+    li = []
+    save_to_json_file(li, "add_item.json")
+else:
+    s = load_from_json_file("add_item.json")
+    s += sys.argv[1:]
+    save_to_json_file(s, "add_item.json")
