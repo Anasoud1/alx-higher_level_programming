2aac8653dc2138d57a071f15d443c280db31e6cb
Anasoud1 <anasoudsassi@gmail.com>
2023-10-16 15:14:04 +0100

    adding from_json_string function in base file

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -16,14 +16,14 @@ class Base:
             Base.__nb_objects += 1
             id = Base.__nb_objects
         self.id = id
-    
+
     @staticmethod
     def to_json_string(list_dictionaries):
         '''returns the JSON string representation of list_dictionaries'''
         if list_dictionaries is None or not list_dictionaries:
             return "[]"
         return json.dumps(list_dictionaries)
-    
+
     @classmethod
     def save_to_file(cls, list_objs):
         '''writes the JSON string representation of list_objs to a file'''
@@ -32,3 +32,10 @@ class Base:
             new.append(obj.to_dictionary())
         with open("{}.json".format(cls.__name__), "w", encoding="utf-8") as f:
             f.write(cls.to_json_string(new))
+
+    @staticmethod
+    def from_json_string(json_string):
+        ''' returns the list of the JSON string representation json_string'''
+        if not json_string or json_string is None:
+            return []
+        return json.loads(json_string)
