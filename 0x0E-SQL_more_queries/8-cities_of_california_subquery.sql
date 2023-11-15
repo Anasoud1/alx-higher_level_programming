db9306cb4047280526dbee1e2aef7fdc27a951f9
Anasoud1 <anasoudsassi@gmail.com>
2023-11-15 17:28:14 +0100

    adding 8-cities_of_california_subquery.sql file

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,4 +1,4 @@
 -- lists all the cities of California that can be found in the database hbtn_0d_usa.
 SELECT id, name FROM hbtn_0d_usa.cities
-WHERE state_id = SELECT * FROM hbtn_0d_usa.states WHERE name = "California"
+WHERE state_id = (SELECT id FROM hbtn_0d_uda.states WHERE name = "California")
 ORDER BY id;
