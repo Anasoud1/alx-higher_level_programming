85d104cf77bdd3e416445a1b2640df0e7e32b3cc
Anasoud1 <anasoudsassi@gmail.com>
2023-12-15 11:46:20 +0100

    chmod +x sql file

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -4,16 +4,17 @@ import MySQLdb
 import sys
 
 
-connection = MySQLdb.connect(host="localhost", user=sys.argv[1],
-                             passwd=sys.argv[2], db=sys.argv[3],
-                             port=3306)
+if __name__ == "__main__":
+    connection = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                                 passwd=sys.argv[2], db=sys.argv[3],
+                                 port=3306)
 
-cursor = connection.cursor()
-cursor.execute("SELECT * FROM states")
-rows = cursor.fetchall()
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM states")
+    rows = cursor.fetchall()
 
-for row in rows:
-    print(row)
+    for row in rows:
+        print(row)
 
-cursor.close()
-connection.close()
+    cursor.close()
+    connection.close()
--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -4,18 +4,18 @@ hbtn_0e_0_usa'''
 import MySQLdb
 import sys
 
+if __name__ == "__main__":
+    connection = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                                 passwd=sys.argv[2], db=sys.argv[3],
+                                 port=3306)
 
-connection = MySQLdb.connect(host="localhost", user=sys.argv[1],
-                             passwd=sys.argv[2], db=sys.argv[3],
-                             port=3306)
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM states")
+    rows = cursor.fetchall()
 
-cursor = connection.cursor()
-cursor.execute("SELECT * FROM states")
-rows = cursor.fetchall()
+    for row in rows:
+        if row[1][0] == "N":
+            print(row)
 
-for row in rows:
-    if row[1][0] == "N":
-        print(row)
-
-cursor.close()
-connection.close()
+    cursor.close()
+    connection.close()
