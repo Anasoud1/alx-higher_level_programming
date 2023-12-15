6a9df0d9034a219725eb6ba026bafb51d1b41f08
Anasoud1 <anasoudsassi@gmail.com>
2023-12-15 18:03:10 +0100

    adding model_city.py, 14-model_city_fetch_by_state.py files

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -6,13 +6,14 @@ from sqlalchemy.orm import sessionmaker
 import sys
 
 
-engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                       .format(sys.argv[1], sys.argv[2], sys.argv[3]))
-Base.metadata.create_all(engine)
-Session = sessionmaker(bind=engine)
-session = Session()
-for instance in session.query(State.name, City.id, City.name).filter(
-                State.id == City.state_id):
-    print("{}: ({}) {}".format(instance[0], instance[1], instance[2]))
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    for instance in session.query(State.name, City.id, City.name).filter(
+                    State.id == City.state_id):
+        print("{}: ({}) {}".format(instance[0], instance[1], instance[2]))
 
-session.close()
+    session.close()
