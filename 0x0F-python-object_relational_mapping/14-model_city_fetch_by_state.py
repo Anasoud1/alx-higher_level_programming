3132d943c0729281443bbb9118f16da414c57c95
Anasoud1 <anasoudsassi@gmail.com>
2023-12-15 18:08:15 +0100

    fixing pycodestyle

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+'''prints the State object with the name passed as argument from the
+database'''
 from model_state import Base, State
 from model_city import City
 from sqlalchemy import create_engine
