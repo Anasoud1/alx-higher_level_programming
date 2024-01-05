c9461daf420d0cb60689a8d3d2a545ce515692ad
Anasoud1 <anasoudsassi@gmail.com>
2024-01-05 18:49:01 +0100

    adding 6-peak files

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -4,6 +4,6 @@
 
 def find_peak(list_of_integers):
     """function that find a peak in a list"""
-    if not list_of_integers:
+    if list_of_integers is None or not list_of_integers:
         return None
     return max(list_of_integers)
