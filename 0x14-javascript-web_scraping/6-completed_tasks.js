f1cddfab180f4694010ecdd89deff04a729fc5db
Anasoud1 <anasoudsassi@gmail.com>
2024-01-30 18:19:51 +0100

    updating 6-completed_tasks.js file

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,29 +1,21 @@
 #!/usr/bin/node
 const request = require('request');
 const url = process.argv[2];
-let j = 0;
-let i = 0;
-let count;
 const dic = {};
 
 request.get(url, (err, response, body) => {
   if (err) {
     console.error(err);
   } else {
-    const datas = JSON.parse(body);
-    while (i < datas.length) {
-      j += 1;
-      count = 0;
-      while (datas[i].userId === j && i !== datas.length - 1) {
-        if (datas[i].completed === true) {
-          count += 1;
+    const todos = JSON.parse(body);
+    for (const todo of todos) {
+      if (todo.completed === true) {
+        if (dic[todo.userId] === undefined) {
+          dic[todo.userId] = 1;
+        } else {
+          dic[todo.userId]++;
         }
-        i += 1;
       }
-
-      dic[j] = count;
-      if (i === datas.length - 1) break;
-      // i += 1
     }
     console.log(dic);
   }
