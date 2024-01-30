ab27b43bb6330a86fdc5fac8cccb92a9eef5e749
Anasoud1 <anasoudsassi@gmail.com>
2024-01-30 16:00:15 +0100

    updating

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -2,14 +2,13 @@
 const request = require('request');
 const url = process.argv[2];
 const pId = 'https://swapi-api.alx-tools.com/api/people/18/';
-let datas;
 let i = 0;
 
 request.get(url, (err, response, body) => {
   if (err) {
     console.error(err);
   } else {
-    datas = JSON.parse(body);
+    const datas = JSON.parse(body);
     for (const data of datas.results) {
       if (data.characters.includes(pId)) {
         i += 1;
