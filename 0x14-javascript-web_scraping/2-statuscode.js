7f3fe5a3ad1e9663fd957c29c012fadb40001c56
Anasoud1 <anasoudsassi@gmail.com>
2024-01-30 13:58:49 +0100

    updating 2-statuscode.js file

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -2,10 +2,10 @@
 const request = require('request');
 const url = process.argv[2];
 
-request(url, (err, response, body) => {
+request.get(url, (err, response, body) => {
   if (err) {
     console.error(err);
   } else {
-    console.log('code: ', response.statusCode);
+    console.log(`code: ${response.statusCode}`);
   }
 });
