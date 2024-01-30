906fa936f5d8bf965492190bfd4d42fd525e6ff1
Anasoud1 <anasoudsassi@gmail.com>
2024-01-30 15:06:19 +0100

    adding 4-starwars_count.js file

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+const request = require('request');
+const url = process.argv[2];
+const pId = 'https://swapi-api.alx-tools.com/api/people/18/';
+let datas;
+let i = 0;
+
+request.get(url, (err, response, body) => {
+  if (err) {
+    console.error(err);
+  } else {
+    datas = JSON.parse(body);
+    for (const data of datas.results) {
+      for (const c of data.characters) {
+        if (c === pId) {
+          i += 1;
+        }
+      }
+    }
+    console.log(i);
+  }
+});
