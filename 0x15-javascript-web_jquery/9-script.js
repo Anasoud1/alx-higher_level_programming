4e82955c43c466749b68ef6d2b3bfd80cc92889d
Anasoud1 <anasoudsassi@gmail.com>
2024-02-07 16:48:15 +0100

    adding 9-script.js file

---
+++ 0x15-javascript-web_jquery/9-script.js
@@ -0,0 +1,11 @@
+$.ajax({
+  url: 'https://hellosalut.stefanbohacek.dev/?lang=fr',
+  method: 'GET',
+  success: function (response) {
+    $('#hello').text(response.hello);
+  },
+
+  error: function (xhr, status, error) {
+    console.error(error);
+  }
+});
